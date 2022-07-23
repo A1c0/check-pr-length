1eab3856ccc02395d8b7be2974a95c7eee4191b2
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2022-07-23 15:47:40 +0200

    ✨ exit if something in stage

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-'use strict';
-const args = require('minimist')(process.argv.slice(2));
-const shell = require('shelljs');
+"use strict";
+const args = require("minimist")(process.argv.slice(2));
+const shell = require("shelljs");
 const chalk = require("chalk");
 const fs = require("fs");
-const version = require('./package.json').version;
+const version = require("./package.json").version;
 
-if (!shell.which('git')) {
-  shell.echo(chalk.red('Sorry, this script requires git'));
+if (!shell.which("git")) {
+  shell.echo(chalk.red("Sorry, this script requires git"));
   shell.exit(1);
 }
-
 
 if (args.version || args.v) {
   shell.echo(`v${version}`);
@@ -18,23 +17,29 @@ if (args.version || args.v) {
 }
 
 if (args.help || args.h) {
-  shell.echo(fs.readFileSync('./ressource/usage.txt', 'utf8'));
+  shell.echo(fs.readFileSync("./ressource/usage.txt", "utf8"));
   shell.exit(0);
 }
 
 // todo check if in git repo : git rev-parse --show-toplevel
-const isInRepoGit = shell.exec('git rev-parse --show-toplevel', {silent:true}).code ===0;
+const isInRepoGit =
+  shell.exec("git rev-parse --show-toplevel", { silent: true }).code === 0;
 
 if (!isInRepoGit) {
-  shell.echo(chalk.red('Sorry, this script must be run in a git repository'));
+  shell.echo(chalk.red("Sorry, this script must be run in a git repository"));
   shell.exit(1);
 }
 
+const isSomethingToCommit =
+  shell.exec("git status --porcelain", { silent: true }).stdout.length > 0;
 
-// todo switch command/action
+if (isSomethingToCommit) {
+  shell.echo(chalk.red("Sorry, there are changes in stage"));
+  shell.exit(1);
+}
 
-const baseBranch = args.base ?? 'develop';
+const baseBranch = args.base ?? "develop";
+
 const maxLines = args.max ?? 500;
 
 console.log(args);
-
